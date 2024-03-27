ed428197eb64745e3d877ffc02eabfc7178b2821
Aman Pandia <pandiaaman16@gmail.com>
2024-03-27 22:24:25 +0530

    Adding first commit to the git

---
+++ src/test/java/com/pandiaaman/graphql/GraphqlSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandiaaman.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
